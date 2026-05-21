55a2d509199dbf48e8716b9946665b43b9fab0f0
h311bo1 <zakiehfaisal@gmail.com>
2026-05-21 12:13:29 +0100

    creating animal

---
+++ CreatingAnimals.js
@@ -0,0 +1,35 @@
+// creating animals 
+// Using factory method
+
+class lion {
+    constructor() {
+        this.animal
+            animal = "lion"
+    
+    }
+}
+
+class cat {
+    constructor() {
+        this.animal
+            animal = "cat"
+    
+    }
+}
+
+class AnimalFactory {
+    static CreateAnimal(type){
+        switch(type) {
+            case animal: "lion"
+                return new lion ();
+
+            case animal: "cat"
+                return new cat ();       
+        }
+    }
+}
+
+//?????
+    //idk man 
+
+
