8fbe4befa9ed5eec98c1180d872dc2d860f74c19
h311bo1 <zakiehfaisal@gmail.com>
2026-05-21 11:24:09 +0100

    Initial commit

---
+++ PricingStrategy.js
@@ -0,0 +1,69 @@
+//price starategy
+//Using strategy method 
+class PricingStrategy {
+  calculatePrice() {}
+}
+
+
+//adult price 
+class AdultPricing extends PricingStrategy {
+  calculatePrice() {
+    return 20;
+  }
+}
+
+//senior price
+class SeniorPricing extends PricingStrategy {
+  calculatePrice() {
+    return 15;
+
+  }
+}
+
+//student price 
+class StudentPricing extends PricingStrategy {
+  calculatePrice() {
+    return 10;
+  }
+}
+
+//price for kids
+class ChildPricing extends PricingStrategy {
+  calculatePrice() {
+    return 5;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//LOG IN (SINGLTON)
+class logger {
+
+
+
+}
+
+class program {
+
+    static() {
+        return("app started");
+    }
+}
